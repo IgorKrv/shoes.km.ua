dd064881ac52d6e128d82f1946259c237fdbd4f5
IgorKrv <2922617472@ukr.net>
2019-05-15 00:58:28 +0300

    add model item add fixed error

--- db/migrate/20190514212728_create_items.rb
+++ db/migrate/20190514212728_create_items.rb
@@ -1,7 +1,10 @@
 class CreateItems < ActiveRecord::Migration[5.0]
   def change
     create_table :items do |t|
-
+      t.float :pric
+      t.float :artikul
+      t.string :name 
+      t.boolean :real
       t.timestamps
     end
   end
